2af04a20ad3041c013b63773a436ef5cdfff8fcd
Olvera Orozco Miguel Ángel <miguel.olveramc@gmail.com>
2023-07-12 09:22:42 -0600

    12/07/2023 fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxGaugeModule } from 'ngx-gauge';
+import {SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { ViewDataComponent } from './components/view-data/view-data.component';
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGaugeModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
